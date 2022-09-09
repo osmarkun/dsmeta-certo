a2b552687e2fda1099fa3a36fcd29cb64637068f
jhonata Henrique <jhohenrique8@gmail.com>
2022-09-09 14:28:11 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-  
+
   return (
 
-    <h1> Ola mundo</h1>
-
+    <>
+      <h1> Ola mundo</h1>
+      <NotificationButton />
+    </>
   )
 
 }
