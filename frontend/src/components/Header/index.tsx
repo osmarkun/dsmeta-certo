9736ef4164049392d3291395077e03c9d5642f0d
jhonata Henrique <jhohenrique8@gmail.com>
2022-09-09 15:13:39 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import logo from "../../assets/img/logo.svg"
+import "./styles.css"
+function Header() {
+
+    return (
+
+        <header>
+
+            <div className="dsmeta-logo-conteiner">
+
+                <img src={logo} alt="DSMeta" />
+
+                <h1> DSMeta </h1>
+
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/um_noob_/">
+                        @um_noob_
+                    </a>
+                </p>
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
